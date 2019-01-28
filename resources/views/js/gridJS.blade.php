30c10d0f9c21abfdcc54254197beeb76dedf9a82
clownprincejoker <jokerjamil007@gmail.com>
2019-01-28 20:59:10 +0600

    CSE299 Project start

---
+++ resources/views/js/gridJS.blade.php
@@ -0,0 +1,43 @@
+var grid = new Konva.Layer();
+
+for(var lw=10; lw<width; lw=lw+10){
+  var line = new Konva.Line({
+      points: [lw, 0, lw, height],
+      stroke: 'lightgrey',
+      strokeWidth: 1,
+  });
+
+  grid.add(line);
+}
+
+for(var lh=10; lh<height; lh=lh+10){
+  var line = new Konva.Line({
+      points: [0, lh, width, lh],
+      stroke: 'lightgrey',
+      strokeWidth: 1,
+  });
+
+  grid.add(line);
+}
+
+for(var lw=10; lw<width; lw=(lw+60)){
+  var line = new Konva.Line({
+      points: [lw, 0, lw, height],
+      stroke: 'lightgrey',
+      strokeWidth: 1,
+  });
+
+  grid.add(line);
+}
+
+for(var lh=10; lh<height; lh=(lh+60)){
+  var line = new Konva.Line({
+      points: [0, lh, width, lh],
+      stroke: 'lightgrey',
+      strokeWidth: 1,
+  });
+
+  grid.add(line);
+}
+
+stage.add(grid);
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+        <meta name="description" content="Online simulator for logic circuits">
+        <meta name="author" content="LSG">
+        <link rel="icon" type="image/png" href="{{ asset('storage/images/devil.ico') }}">
+
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>Logic Simulator</title>
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script src="https://unpkg.com/konva@2.4.2/konva.min.js"></script>
+
+        <!-- Fonts -->
+        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+
+    <body>
+      <div id="gridbox" style="margin: auto; width: 50%; overflow-y: scroll; height:400px;">
+        <div id="container"></div>
+      </div>
+
+      <script>
+        var width = document.getElementById('gridbox').offsetWidth*5;
+        var height = document.getElementById('gridbox').offsetHeight*5;
+
+        var stage = new Konva.Stage({
+            container: 'container',
+            width: width,
+            height: height,
+        });
+
+        var tempLayer = new Konva.Layer();
+        stage.add(tempLayer);
+
+        @include('js.gridJS')
+
+        var layer = new Konva.Layer();
+
+        var box = new Konva.Rect({
+            name:'sampleBox',
+            x: 20,
+            y: 20,
+            width: 20,
+            height: 30,
+            stroke: 'gray',
+            strokeWidth: 1,
+            dash: [10, 5]
+        });
+
+        // add cursor styling
+        box.on('mouseover', function() {
+            document.body.style.cursor = 'pointer';
+            this.stroke('darkgrey');
+            this.strokeWidth(3);
+            layer.draw();
+        });
+        box.on('mouseout', function() {
+            document.body.style.cursor = 'default';
+            this.stroke('grey');
+            this.strokeWidth(1);
+            layer.draw();
+        });
+        layer.add(box);
+
+        var i=0;
+        box.on('click', function () {
+            i++;
+            var b = new Konva.Rect({
+                name:'newBox'+i,
+                x: 100,
+                y: 20,
+                width: 40,
+                height: 50,
+                stroke: 'gray',
+                strokeWidth: 1,
+                draggable:true,
+                dash: [10, 5]
+            });
+
+            // add cursor styling
+            b.on('mouseover', function() {
+                document.body.style.cursor = 'grab';
+                this.stroke('darkgrey');
+                this.strokeWidth(3);
+                layer.draw();
+            });
+            b.on('mouseout', function() {
+                document.body.style.cursor = 'default';
+                this.stroke('grey');
+                this.strokeWidth(1);
+                layer.draw();
+            });
+            b.on('mousedown touchstart', function () {
+                document.body.style.cursor = 'grabbing';
+            });
+            b.on('mouseup touchend', function () {
+                document.body.style.cursor = 'grab';
+            });
+
+            b.on('click', function (evt) {
+              var shape = evt.target;
+              alert('you clicked on \"' + shape.getName() + '\"');
+            });
+
+            layer.add(b);
+            layer.draw();
+        });
+
+        stage.on("dragend", function(e){
+            var pos = stage.getPointerPosition();
+            var shape = layer.getIntersection(pos);
+            if (shape) {
+                previousShape.fire('drop', {
+                    type : 'drop',
+                    target : previousShape,
+                    evt : e.evt
+                }, true);
+            }
+            previousShape = undefined;
+            e.target.moveTo(layer);
+            layer.draw();
+            tempLayer.draw();
+        });
+
+
+        stage.add(layer);
+      </script>
+    </body>
+</html>
